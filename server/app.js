5a94477fc7cc38d97678a126a76c40166742f14b
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-12-22 12:36:41 +0700

    Root query

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema/schema';
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema }));
 
 app.listen(4000, () => {
   console.log('now listen on port 4000');
